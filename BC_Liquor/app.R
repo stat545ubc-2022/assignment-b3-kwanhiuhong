bb55e2d3f51cbda2335087d05c3bff8ae59f99fe
Terry Kwan <hiuhongkwan@gmail.com>
2022-12-01 18:09:26 -0800

    Finished Assignment 4

--- BC_Liquor/app.R
+++ BC_Liquor/app.R
@@ -10,35 +10,50 @@
 library(shiny)
 library(tidyverse)
 library(shinythemes)
-
+library(DT)
 bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
 
 # Define the UI for BC Liquid Store
 ui <- fluidPage(
+  
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "https://fonts.cdnfonts.com/css/cute")
+  ),
+  
+  # Feature 4 - Added a special font using an external css, which helps make our webpage a bit nicer
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+  ),
   
   # Feature 1 - Added a new theme for the shinny app - One change to UI, this helps enhance the UI because people might be bored with the default UI
   theme = shinytheme("darkly"),
   
   # Feature 3 - Added a UBC logo in the title panel - Another change to UI, this helps users identify where the developers are and can better protect the property right
   titlePanel(title = div(img(width = "20%", src="UBC_logo.png"), "BC Liquor Store prices by UBC students")),
-  
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("priceInput", "Price", min = 0, max = 100,
-                  value = c(25, 40), pre = "$"),
-      radioButtons("typeInput", "Product type",
-                   choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
-                   selected = "WINE"),
-      uiOutput("countryOutput"),
-      
-      # Feature 2 - Allow user to sort the data by Alcohol_Content - One new functional widget that modifies at least one output. This lets user find out which alcohol is more suitable for them.
-      checkboxInput("sortByAlcoholContent", "Sort data by alcoholic content", TRUE)
-    ),
-    mainPanel(
-      plotOutput("coolplot"),
-      br(), br(),
-      tableOutput("results")
-    )
+  tabsetPanel(
+    tabPanel("Overview", sidebarLayout(
+      sidebarPanel(
+        sliderInput("priceInput", "Price", min = 0, max = 100,
+                    value = c(25, 40), pre = "$"),
+        radioButtons("typeInput", "Product type",
+                     choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
+                     selected = "WINE"),
+        uiOutput("countryOutput"),
+        
+        # Feature 2 - Allow user to sort the data by Alcohol_Content - One new functional widget that modifies at least one output. This lets user find out which alcohol is more suitable for them.
+        checkboxInput("sortByAlcoholContent", "Sort data by alcoholic content", TRUE)
+      ),
+      mainPanel(
+        plotOutput("coolplot"),
+        br(), br(),
+        # Feature 6 - Allow user to export/download the dataset in CSV format so that they can process it on Excel.
+        downloadButton("exportCSV"),
+        tableOutput("results")
+      )
+    )),
+    
+    # Feature 5 - We made a static plot to allow users to view how many alcohol we have according to different alcohol types, which gives them a general overview about our current stock.
+    tabPanel("Overview By Types of Alcohol", plotOutput("plotbytype")),
   )
 )
 
@@ -83,6 +98,23 @@ server <- function(input, output) {
   output$results <- renderTable({
     filtered()
   })
+  
+  output$plotbytype <- renderPlot({
+    ggplot(bcl, aes(x = factor(1), fill = Type)) +
+      geom_bar() +
+      labs(y= "Number", x ="") + 
+      coord_polar("y")
+  })
+  
+  # Feature 6 - Allow user to export/download the dataset in CSV format so that they can process it on Excel.
+  output$exportCSV <- downloadHandler(
+    filename = function() {
+      "bcl-results.csv"
+    },
+    content = function(file) {
+      write.csv(filtered(), file)
+    }
+  )
 }
 
 # Run the application 
--- README.md
+++ README.md
@@ -1,17 +1,32 @@
-# STAT545B - Assignment B-3
+# STAT545B - Assignment B-3 and Assignment 4
 
-This repo holds my shinny app for STAT545B - Assignment B-3
+This repo holds my shinny app for STAT545B - Assignment B-3 and Assignment 4
 <br><br>
+For Assignment 3
 I chose Option A - BC Liquor app and made the application more UI/UX friendly by changing the background theme and adding a UBC logo in the title to acknolwedge our developers' contribution. Also, we included a new feature to sort the alcohol data by alcoholic content, which helps customers to find the best alcohol that suits their needs the best.
 <br><br>
-I deployed my app at https://terrykwan.shinyapps.io/BC_Liquor/
+For Assignment 4
+I chose Option C - Shinny App. I made some more improvements based on the Shinny app I made in assignment B-3, including adding an external css, a new plot and a download button.
+<br><br>
+Version for Assignment 3: I deployed my app at https://terrykwan.shinyapps.io/BC_Liquor/ <br>
+Version for Assignment 4: I deployed my app at https://terrykwan.shinyapps.io/BC_Liquor_Assignment_4/
 <br><br>
 I acknowledge that the data we use comes from OpenDataBC which contains information about all the products sold by BC Liquor Store.
 
 ### Features
+
+For Assignment 3:
 
 1. Changed the background theme to "darkly", which is a dark mode and friendly to your eyes.
 
 2. Allow users to sort the alcohol data by alcoholic content, for better UX.
 
 3. Added a UBC logo on the title, so that people know who developed this shinny app.
+
+For Assignment 4:
+
+4. Added a special font using an external css, which helps make our webpage a bit nicer
+
+5. Added a static plot to allow users to view how many alcohol we have according to different alcohol types, which gives them a general overview about our current stock.
+
+6. Allow user to export/download the dataset in CSV format so that they can process it on Excel.
